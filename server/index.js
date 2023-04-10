1a321c7f7b6e812233d02fbca44d36d7f7991b63
Ezechiel Iti <papaezechiel@sympatico.ca>
2023-04-09 22:47:47 -0400

    mongoDB created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
-import { Express } from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
-const app = Express();
+const app = express();
 app.use(cors());
-app.use(Express.json({ Limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async (req, res) => {
-  res.send('Hello from EZE-AI IMAGE!')
-})
+app.get("/", async (req, res) => {
+  res.send("Hello from EZE-AI IMAGE!");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
