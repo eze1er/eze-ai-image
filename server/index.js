85092a91c95ba090393a6ab50e133fd054e078b6
Ezechiel Iti <papaezechiel@sympatico.ca>
2023-04-10 01:06:39 -0400

    server side connected

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import ezeRoutes from "./routes/ezeRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/eze", ezeRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from EZE-AI IMAGE!");
